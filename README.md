c7726df442db67f522aeeddb219b00fa6877aa54
wk-j <somnuk.wk@gmail.com>
2019-12-24 20:08:58 +0700

    Postgres

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ## Group
 
+- [GroupBy or GroupJoin](https://github.com/aspnet/EntityFrameworkCore/issues/17068)
+- [Entity Framework Core 3.0 - "Hidden" GROUP BY Capabilities (Part 1)](https://www.thinktecture.com/entity-framework/entity-framework-core-30-hidden-group-by-capabilities-part-1)
+
 - http://localhost:5000/api/group/insert
 - http://localhost:5000/api/group/group
+- http://localhost:5000/api/group/group2
--- src/GroupBy/Controllers/GroupController.cs
+++ src/GroupBy/Controllers/GroupController.cs
@@ -4,30 +4,32 @@ using System.Linq;
 using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 
-namespace GroupBy.Controllers
-{
-
+namespace GroupBy.Controllers {
     [Route("api/[controller]/[action]")]
-    public class GroupController : ControllerBase
-    {
+
+    public class GroupController : ControllerBase {
         ILoggerFactory fact;
-        public GroupController(ILoggerFactory factory)
-        {
+        public GroupController(ILoggerFactory factory) {
             this.fact = factory;
         }
 
-        DbContextOptions Options()
-        {
+        DbContextOptions Options() {
+            // DbContextOptions options = new DbContextOptionsBuilder()
+            //     .UseMySql("Host=localhost;User Id=root; Password=1234;Database=App")
+            //     .UseLoggerFactory(fact)
+            //     .Options;
+
             DbContextOptions options = new DbContextOptionsBuilder()
-                .UseMySql("Host=localhost;User Id=root; Password=1234;Database=App")
+                .UseNpgsql("Host=localhost;User Id=root; Password=1234;Database=app")
                 .UseLoggerFactory(fact)
                 .Options;
+
+
             return options;
         }
 
         [HttpGet]
-        public int Insert()
-        {
+        public int Insert() {
             var options = Options();
             using var context = new MyContext(options);
             context.Database.EnsureDeleted();
@@ -59,13 +61,27 @@ namespace GroupBy.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Student> Group()
-        {
+        public IEnumerable<Student> Group() {
             var options = Options();
             using var context = new MyContext(options);
             var student = context.Students
                 .GroupBy(x => x.Course)
-                .Select(x => x.FirstOrDefault())
+                .Select(x => x.OrderByDescending(x => x.Score).ToList().First())
+                .ToList();
+
+            return student;
+        }
+
+        [HttpGet]
+        public IEnumerable<dynamic> Group2() {
+            var options = Options();
+            using var context = new MyContext(options);
+            var student = context.Students
+                .GroupBy(x => x.Course)
+                .Select(x => new {
+                    Key = x.Key,
+                    Count = x.Count()
+                })
                 .ToList();
 
             return student;
